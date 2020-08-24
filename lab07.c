0550ced76d5ebc14f8d999b48ddfa7b4aa8af4ed
Weingart <1151591791157002@mil>
2020-08-23 18:32:03 -0600

    added .gitignore

--- lab07.c
+++ lab07.c
@@ -20,4 +20,10 @@ int main() {
     // TODO 1.0
     // Call cubeIt() with a value of 3 and print the result
 
+
+    // The following line prevents the debugging/run window
+    // from immediately closeing.  You will need to hit enter
+    // to close the terminal/cmd window
+    char c = '\0';scanf("%c", &c);
+    return 0;
 }
